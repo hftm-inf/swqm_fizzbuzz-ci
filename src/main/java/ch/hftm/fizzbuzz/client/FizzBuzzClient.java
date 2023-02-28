2b9f1ab0ac2f71d8d250f0e2032accca24adf936
simeon.liniger <simeon.liniger@hftm.ch>
2023-02-28 15:14:12 +0100

    fix: compilation error

--- src/main/java/ch/hftm/fizzbuzz/client/FizzBuzzClient.java
+++ src/main/java/ch/hftm/fizzbuzz/client/FizzBuzzClient.java
@@ -7,20 +7,16 @@ public class FizzBuzzClient {
 	FizzBuzzService fizzBuzzService = new FizzBuzzService();
 
 	public String getResultForScope(int beginn, int end) {
-		String result = "";
+		StringBuilder result = new StringBuilder();
 		
 		for (int i = beginn; i <= end; i++) {
-            result += fizzBuzzService.getResultOfNumber(i) + ", ";
-			nothing = "ugly";
-			nothing += "er";
+            result.append(fizzBuzzService.getResultOfNumber(i) + ", ");
         }
 		if (!result.isEmpty()) {
-			result = result.substring(0, result.length()-2);
-			nothing = "ugly";
-			nothing += "er";
+			return result.substring(0, result.length()-2);
 		}
 		
-		return result;
+		return result.toString();
 	}
 
 }
